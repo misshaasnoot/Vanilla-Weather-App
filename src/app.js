1d21dba2c8f6409ef4d1ce8f65fe2be6c195983f
sophie Haasnoot <ms.haasnoot@gmail.com>
2023-02-12 11:38:31 +0100

    working on API

---
+++ src/app.js
@@ -0,0 +1,52 @@
+let now = new Date();
+
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+
+  let currentDate = `${day}, ${hours}:${minutes}`;
+  let dateHeader = document.querySelector("#current-date");
+  dateHeader.innerHTML = currentDate;
+
+
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#the-city");
+  let conditionsElement = document.querySelector("#conditions");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+
+
+
+
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  cityElement.innerHTML = response.data.city;
+  conditionsElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+
+
+
+  console.log(response.data);
+
+}
+
+let apiKey = "b56759ebd8a5t745b7782e0304a5ff9o";
+let apiURL = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${apiKey}&units=metric`;
+
+axios.get(apiURL).then(displayTemperature);
